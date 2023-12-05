a91d3e5fb52e4fbfca438b47a1d7f0d94daa3b80
Jose M Dev <josemiguelordonez99@gmail.com>
2023-12-05 18:10:43 -0500

    modificaciones

--- index.php
+++ index.php
@@ -48,7 +48,7 @@ if ($_POST['METHOD'] == 'PUT') {
     $direccion = $_POST['direccion'];
     
 
-    $query = "update cliente set id_tipo_documento='$id_tipo_documento', numero_documento='$numero_documento', nombre='$nombre', apellido='$apellido', telefono='$telefono',correo_electronico='$correo_electronico',direccion='$direccion' where id_cliente='$id_cliente'";
+    $query = "update cliente set id_tipo_documento='$id_tipo_documento', numero_documento='$numero_documento', telefono='$telefono', direccion='$direccion', correo_electronico='$correo_electronico', nombre='$nombre', apellido='$apellido' where id_cliente='$id_cliente'";
     $result = metodoPut($query);
     echo json_encode($result);
     header('HTTP/1.1 200 0K');
