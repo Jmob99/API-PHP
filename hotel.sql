5d0d3fa08caee8187a1903bce78a2096bfec781b
Jose M Dev <josemiguelordonez99@gmail.com>
2023-12-07 16:28:22 -0500

    tabla cliente workinggit add .

--- hotel.sql
+++ hotel.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1:3306
--- Tiempo de generación: 12-10-2023 a las 16:39:37
--- Versión del servidor: 5.7.36
--- Versión de PHP: 7.4.26
+-- Tiempo de generación: 07-12-2023 a las 21:21:07
+-- Versión del servidor: 8.0.31
+-- Versión de PHP: 8.0.26
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -29,26 +29,26 @@ SET time_zone = "+00:00";
 
 DROP TABLE IF EXISTS `cliente`;
 CREATE TABLE IF NOT EXISTS `cliente` (
-  `id_cliente` int(11) NOT NULL AUTO_INCREMENT,
-  `id_tipo_documento` int(11) NOT NULL,
-  `numero_documento` bigint(20) NOT NULL,
-  `telefono` bigint(20) NOT NULL,
+  `id_cliente` int NOT NULL AUTO_INCREMENT,
+  `id_tipo_documento` int NOT NULL,
+  `numero_documento` bigint NOT NULL,
+  `telefono` bigint NOT NULL,
   `direccion` varchar(255) DEFAULT NULL,
   `correo_electronico` varchar(255) DEFAULT NULL,
   `nombre` varchar(255) DEFAULT NULL,
   `apellido` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id_cliente`),
   KEY `id_tipo_documento` (`id_tipo_documento`)
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Volcado de datos para la tabla `cliente`
 --
 
 INSERT INTO `cliente` (`id_cliente`, `id_tipo_documento`, `numero_documento`, `telefono`, `direccion`, `correo_electronico`, `nombre`, `apellido`) VALUES
-(1, 1, 123345, 25165, 'kr 25', 'sara@hotmail.es', 'Sara', 'Castaño'),
-(2, 3, 15165, 121, 'cl 2 ', 'mike@outlook.com', 'mike', 'myers'),
-(3, 2, 165486, 2213156, 'Clle 10 50', 'pedro@hotmail.com', 'Pedro', 'Castro');
+(1, 1, 12345, 310320, 'kr 20 # 18', 'jm@hotmail.com', 'Jose', 'Ordoñez'),
+(2, 1, 456789, 310310, 'cl 6 # 8', 'vahos@hotmail.com', 'Jonathan', 'Vahos'),
+(3, 2, 11234, 500800, 'manzana 2', 'cindygil@gmail.com', 'Cindy', 'Gil');
 
 -- --------------------------------------------------------
 
@@ -58,8 +58,8 @@ INSERT INTO `cliente` (`id_cliente`, `id_tipo_documento`, `numero_documento`, `t
 
 DROP TABLE IF EXISTS `empleado`;
 CREATE TABLE IF NOT EXISTS `empleado` (
-  `id_empleado` int(11) NOT NULL AUTO_INCREMENT,
-  `id_tipo_documento` int(11) NOT NULL,
+  `id_empleado` int NOT NULL AUTO_INCREMENT,
+  `id_tipo_documento` int NOT NULL,
   `numero_documento` double NOT NULL,
   `telefono` double NOT NULL,
   `direccion` varchar(30) NOT NULL,
@@ -70,7 +70,7 @@ CREATE TABLE IF NOT EXISTS `empleado` (
   `password` varchar(15) NOT NULL,
   PRIMARY KEY (`id_empleado`),
   KEY `id_tipo_documento` (`id_tipo_documento`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -80,10 +80,10 @@ CREATE TABLE IF NOT EXISTS `empleado` (
 
 DROP TABLE IF EXISTS `estado`;
 CREATE TABLE IF NOT EXISTS `estado` (
-  `id_estado` int(11) NOT NULL AUTO_INCREMENT,
+  `id_estado` int NOT NULL AUTO_INCREMENT,
   `nombre` varchar(20) NOT NULL,
   PRIMARY KEY (`id_estado`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -93,18 +93,18 @@ CREATE TABLE IF NOT EXISTS `estado` (
 
 DROP TABLE IF EXISTS `habitacion`;
 CREATE TABLE IF NOT EXISTS `habitacion` (
-  `id_habitacion` int(11) NOT NULL AUTO_INCREMENT,
-  `id_hotel` int(11) NOT NULL,
-  `id_tipo_habitacion` int(11) NOT NULL,
-  `numero` int(11) NOT NULL,
-  `capacidad` int(11) NOT NULL,
+  `id_habitacion` int NOT NULL AUTO_INCREMENT,
+  `id_hotel` int NOT NULL,
+  `id_tipo_habitacion` int NOT NULL,
+  `numero` int NOT NULL,
+  `capacidad` int NOT NULL,
   `precio` double NOT NULL,
   `id_estado` varchar(20) NOT NULL,
   PRIMARY KEY (`id_habitacion`),
   KEY `id_hotel` (`id_hotel`),
   KEY `id_tipo_habitacion` (`id_tipo_habitacion`),
   KEY `id_estado` (`id_estado`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -114,14 +114,14 @@ CREATE TABLE IF NOT EXISTS `habitacion` (
 
 DROP TABLE IF EXISTS `hotel`;
 CREATE TABLE IF NOT EXISTS `hotel` (
-  `id_hotel` int(11) NOT NULL AUTO_INCREMENT,
+  `id_hotel` int NOT NULL AUTO_INCREMENT,
   `nombre` varchar(30) NOT NULL,
   `direccion` varchar(30) NOT NULL,
-  `telefono` bigint(20) NOT NULL,
+  `telefono` bigint NOT NULL,
   `departamento` varchar(20) NOT NULL,
   `ciudad` varchar(20) NOT NULL,
   PRIMARY KEY (`id_hotel`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -131,18 +131,18 @@ CREATE TABLE IF NOT EXISTS `hotel` (
 
 DROP TABLE IF EXISTS `reserva`;
 CREATE TABLE IF NOT EXISTS `reserva` (
-  `id_reserva` int(11) NOT NULL AUTO_INCREMENT,
-  `id_cliente` int(11) NOT NULL,
-  `id_habitacion` int(11) NOT NULL,
+  `id_reserva` int NOT NULL AUTO_INCREMENT,
+  `id_cliente` int NOT NULL,
+  `id_habitacion` int NOT NULL,
   `fecha_reserva` date NOT NULL,
   `fecha_ingreso` date NOT NULL,
   `fecha_salida` date NOT NULL,
-  `cant_ocupantes` int(11) NOT NULL,
+  `cant_ocupantes` int NOT NULL,
   `valor_alojamiento` double NOT NULL,
   PRIMARY KEY (`id_reserva`),
   KEY `id_cliente` (`id_cliente`),
   KEY `id_habitacion` (`id_habitacion`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -152,10 +152,10 @@ CREATE TABLE IF NOT EXISTS `reserva` (
 
 DROP TABLE IF EXISTS `tipo_documento`;
 CREATE TABLE IF NOT EXISTS `tipo_documento` (
-  `id_tipo_documento` int(11) NOT NULL AUTO_INCREMENT,
+  `id_tipo_documento` int NOT NULL AUTO_INCREMENT,
   `nom_tipo_documento` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id_tipo_documento`)
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Volcado de datos para la tabla `tipo_documento`
@@ -174,10 +174,10 @@ INSERT INTO `tipo_documento` (`id_tipo_documento`, `nom_tipo_documento`) VALUES
 
 DROP TABLE IF EXISTS `tipo_habitacion`;
 CREATE TABLE IF NOT EXISTS `tipo_habitacion` (
-  `id_tipo_habitacion` int(11) NOT NULL AUTO_INCREMENT,
+  `id_tipo_habitacion` int NOT NULL AUTO_INCREMENT,
   `nom_tipo_habitacion` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id_tipo_habitacion`)
-) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Volcado de datos para la tabla `tipo_habitacion`
